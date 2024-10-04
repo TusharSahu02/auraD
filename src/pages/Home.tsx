a0875703e131e61f309a8e459d71926dc672c4fd
Tushar Sahu <tusharsahu190702@gmail.com>
2024-10-04 23:53:43 +0530

    refactor : home page done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,100 +2,148 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/common/Home/Navbar";
 const Home = () => {
   return (
-    <>
-      <div className="bg-[#090716] w-full min-h-screen relative">
-        <Navbar />
-        <div className=" py-2 px-10 flex items-center flex-col justify-center z-20 relative ">
-          <div className=" flex items-center flex-col justify-center pt-24 text-white ">
-            <div className="flex items-center gap-x-3">
-              <div className="h-[1px] bg-gradient-to-r from-gray-900 to-[#ffffffaa] w-[78px]"></div>
-              <h1 className=" font-geistLight text-xl text-gray-400  shiny-text">
-                Introducing
-              </h1>
-              <div className="h-[1px] bg-gradient-to-r from-[#ffffffaa] to-gray-900 w-[78px]"></div>
-            </div>
-            <h1 className="shiny-text font-geistBlack text-[100px] mt-1">
-              auraD
+    <div className="bg-[#090716] w-full min-h-screen relative">
+
+      <Navbar />
+      <div className=" py-2 px-10 flex items-center flex-col justify-center z-20 ">
+        <div className=" flex items-center flex-col justify-center pt-24 text-white ">
+          <div className="flex items-center gap-x-3">
+            <div className="h-[1px] bg-gradient-to-r from-gray-900 to-[#ffffffaa] w-[78px]"></div>
+            <h1 className=" font-geistLight text-xl text-gray-400  shiny-text">
+              Introducing
             </h1>
-            <p className="text-4xl mt-10 font-geistMedium capitalize">
-              copy. paste. ship.
-            </p>
-            <p className="text-gray-500 text-center mt-5 w-[35%]">
-              Create stunning React websites in hours, not months, with AuraD
-              UI's sleek and animated components. Optimized for speed and SEO,
-              simply copy, paste, and customize to deliver a high-performance
-              site with ease. 🚀
-            </p>
-
-            <Link to="/docs/components/button" className="mt-[85px]">
-              <div className="border  border-opacity-10 shiny-text border-white/25 px-4 py-2 rounded-full text-sm">
-                Browse Components
-              </div>
-            </Link>
+            <div className="h-[1px] bg-gradient-to-r from-[#ffffffaa] to-gray-900 w-[78px]"></div>
           </div>
+          <h1 className="shiny-text font-geistBlack text-[100px] mt-1">
+            auraD
+          </h1>
+          <p className="text-4xl mt-10 font-geistMedium capitalize">
+            copy. paste. ship.
+          </p>
+          <p className="text-gray-500 text-center mt-5 lg:w-[35%] xl:w-[45%] md:w-1/2">
+            Create stunning React websites in hours, not months, with AuraD UI's
+            sleek and animated components. Optimized for speed, simply copy,
+            paste.
+          </p>
 
-          <div className=" w-[60%] rounded-lg  flex items-center mt-20 flex-col ">
-            <h1 className="text-white mb-8">Components</h1>
+          <Link
+            to="/docs/components/button"
+            className="xl:mt-[85px] z-20  mt-[65px]"
+          >
+            <button className="border text-sm font-medium relative border-white/[0.2]  text-white px-4 py-2 rounded-full group">
+              <span className="absolute inset-x-0 w-1/2 mx-auto  transition-opacity duration-300  -top-px bg-gradient-to-r opacity-0 group-hover:opacity-100  from-transparent via-blue-500 to-transparent  h-px" />
+              <span>Browse Components</span>
+              <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+            </button>
+          </Link>
+        </div>
 
-            <div className="grid grid-cols-3 gap-5 w-[80%]">
-              <div className="border border-white/10 rounded-xl h-40 cursor-pointer"></div>
-              <div className="border border-white/10 rounded-xl h-40 cursor-pointer"></div>
-              <div className="border border-white/10 rounded-xl h-40 cursor-pointer"></div>
-              <div className="border border-white/10 rounded-xl h-40 cursor-pointer"></div>
-              <div className="border border-white/10 rounded-xl h-40 cursor-pointer"></div>
-              <div className="border border-white/10 rounded-xl h-40 cursor-pointer"></div>
+        {/* bento grid components */}
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-5 xl:w-[70%] 2xl:w-[60%]  mt-20">
+          <div className="border border-white/[0.2] group cursor-pointer  col-span-2 flex items-center justify-start flex-col rounded-lg p-3 space-y-2">
+            <div className="border border-white/[0.1] bg-[#1e1e1e3e] overflow-hidden   h-[200px] w-full  rounded-lg">
+              <img
+                src="https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt=""
+                className="w-full h-full object-cover rounded-lg group-hover:scale-[102%] transition-all duration-300"
+              />
             </div>
-
-            <div className="w-[80%] flex items-center justify-center my-6">
-              <Link to="/docs/components/button">
-                <div className="border  border-opacity-10 shiny-text border-white/25 px-4 py-2 rounded-full text-sm">
-                  See more
-                </div>
-              </Link>
+            <div className="px-1 group-hover:pl-3 transition-all duration-300">
+              <h1 className="text-white">Testing</h1>
+              <p className="text-white">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel
+                animi in sunt illum quae deleniti omnis voluptatibus dolorum
+                autem eligendi.
+              </p>
             </div>
-            <h1 className="text-gray-400/60 my-10 text-sm">
-              &copy; 2024
-              <span className="ml-1">
-                <Link to={"/about-me"}>Tushar Sahu</Link>
-              </span>
-              . All rights reserved.
-            </h1>
+          </div>
+          <div className="border border-white/[0.2] group cursor-pointer col-span-2 lg:col-span-1  rounded-lg p-3 flex items-center justify-start flex-col space-y-2">
+            <div className="border border-white/[0.1] bg-[#1e1e1e3e] overflow-hidden   h-[200px] w-full  rounded-lg">
+              <img
+                src="https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt=""
+                className="w-full h-full object-cover rounded-lg group-hover:scale-[102%] transition-all duration-300"
+              />
+            </div>
+            <div className="px-1  group-hover:pl-3 transition-all duration-300">
+              <h1 className="text-white">Testing</h1>
+              <p className="text-white">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel
+                animi in sunt illum quae deleniti omnis voluptatibus dolorum
+                autem eligendi.
+              </p>
+            </div>
+          </div>
+          <div className="border border-white/[0.2] group cursor-pointer col-span-2 md:col-span-1 flex items-center justify-start flex-col space-y-2  rounded-lg p-3">
+            <div className="border border-white/[0.1] bg-[#1e1e1e3e] overflow-hidden   h-[200px] w-full  rounded-lg">
+              <img
+                src="https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt=""
+                className="w-full h-full object-cover rounded-lg group-hover:scale-[102%] transition-all duration-300"
+              />
+            </div>
+            <div className="px-1  group-hover:pl-3 transition-all duration-300">
+              <h1 className="text-white">Testing</h1>
+              <p className="text-white">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel
+                animi in sunt illum quae deleniti omnis voluptatibus dolorum
+                autem eligendi.
+              </p>
+            </div>
+          </div>
+          <div className="border border-white/[0.2] group cursor-pointer  col-span-2 flex items-center justify-start flex-col rounded-lg p-3 space-y-2">
+            <div className="border border-white/[0.1] bg-[#1e1e1e3e] overflow-hidden   h-[200px] w-full  rounded-lg">
+              <img
+                src="https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt=""
+                className="w-full h-full object-cover rounded-lg group-hover:scale-[102%] transition-all duration-300"
+              />
+            </div>
+            <div className="px-1  group-hover:pl-3 transition-all duration-300">
+              <h1 className="text-white">Testing</h1>
+              <p className="text-white">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel
+                animi in sunt illum quae deleniti omnis voluptatibus dolorum
+                autem eligendi.
+              </p>
+            </div>
           </div>
         </div>
-
-        {/* background pattern designs */}
-        <div className="grid grid-cols-4 absolute top-[130px] w-full  left-1/2 -translate-x-1/2">
-          <div className="  h-[80px] "></div>
-          <div className="border-t border-white/10 border-b h-[80px] col-span-2"></div>
-          <div className=" border-white/10  h-[80px]"></div>
-          <div className="border-b border-white/10 h-[130px]"></div>
-          <div className="border-b border-white/10 h-[130px] col-span-2"></div>
-          <div className="border-b border-white/10 h-[130px]"></div>
-          <div className="border-b border-white/10 h-60"></div>
-          <div className="border-b border-white/10 h-60 col-span-2"></div>
-          <div className="border-b border-white/10 h-60"></div>
-          <div className=" border-white/10 h-[80px]"></div>
-          <div className="border-b border-white/10 rounded-3xl h-[80px] col-span-2"></div>
-          <div className=" border-white/10 h-[80px]"></div>
+        <div className="w-[80%] flex items-center justify-center my-6">
+          <Link to="/docs/components/button">
+            <button className="border text-sm font-medium relative border-white/[0.2]  text-white px-4 py-2 rounded-full group">
+              <span className="absolute inset-x-0 w-1/2 mx-auto  transition-opacity duration-300  -top-px bg-gradient-to-r opacity-0 group-hover:opacity-100  from-transparent via-blue-500 to-transparent  h-px" />
+              <span>See more</span>
+              <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+            </button>
+          </Link>
         </div>
-        <div className="grid grid-cols-4  mt-[57px] absolute top-0 w-full  left-1/2 -translate-x-1/2">
-          <div className="h-[603px]"></div>
-          <div className="col-span-2 rounded-b-3xl  border-l border-r border-white/10 grid grid-cols-8 ">
-            <div></div>
-            <div className="border-r  border-l border-white/10 col-span-6"></div>
-          </div>
-          <div className="h-[603px] "></div>
-        </div>
-
-        {/* <div className=" radial size-[90vh] rounded-full blur-[100px] opacity-30 absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div> */}
-        {/* 
-        <div className="hero__spotlights w-full ">
-          <div className="spotlight_spotlight__6lNkv rotate-[15deg] absolute top-[100px] left-1/2 -translate-x-1/2 w-[200px] h-[700px] opacity-60 "></div>
-          <div className="spotlight_spotlight__6lNkv -rotate-[15deg] absolute top-[100px] left-1/2 w-[200px] h-[700px] opacity-60 -translate-x-1/2"></div>
-          <div className="spotlight_spotlight__6lNkv rotate-0 absolute top-[100px] left-1/2 w-[200px] h-[700px] opacity-60 -translate-x-1/2"></div>
-        </div> */}
       </div>
-    </>
+
+      {/* background pattern designs */}
+      <div className="lg:grid grid-cols-4 hidden  absolute top-[130px] w-full   left-1/2 -translate-x-1/2">
+        <div className="  h-[80px] "></div>
+        <div className="border-t border-white/10 border-b h-[80px] col-span-2"></div>
+        <div className=" border-white/10  h-[80px]"></div>
+        <div className="border-b border-white/10 h-[130px]"></div>
+        <div className="border-b border-white/10 h-[130px] col-span-2"></div>
+        <div className="border-b border-white/10 h-[130px]"></div>
+        <div className="border-b border-white/10 h-60"></div>
+        <div className="border-b border-white/10 h-60 col-span-2"></div>
+        <div className="border-b border-white/10 h-60"></div>
+        <div className=" border-white/10 h-[80px]"></div>
+        <div className="border-b border-white/10 rounded-3xl h-[80px] col-span-2"></div>
+        <div className=" border-white/10 h-[80px]"></div>
+      </div>
+      <div className="lg:grid grid-cols-4 hidden  mt-[57px] absolute top-0 w-full  left-1/2 -translate-x-1/2">
+        <div className="h-[603px]"></div>
+        <div className="col-span-2 rounded-b-3xl  border-l border-r border-white/10 grid grid-cols-8 ">
+          <div></div>
+          <div className="border-r  border-l border-white/10 col-span-6"></div>
+        </div>
+        <div className="h-[603px] "></div>
+      </div>
+    </div>
   );
 };
 
