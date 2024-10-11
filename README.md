6576adf46253bcfecb4685c09be9a30455b46d08
Tushar Sahu <tusharsahu190702@gmail.com>
2024-10-11 13:42:26 +0530

    fix : input typescript error

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# auraD
+<div>
+    <h1 style="text-align: center;color: #090716;font-size: 100px">auraD</h1>
+    <p style="text-align: center; margin-top: -20px"> Design. Develop. Deploy.</p>
+</div>
 
-
-<h1></h1>
-
-
+---
--- src/constants/ComponentMap.ts
+++ src/constants/ComponentMap.ts
@@ -1,5 +1,5 @@
 import ButtonDoc from "../pages/docs/Components/ButtonDoc";
-import InputDoc from "../pages/docs/Components/InputDoc";
+// import InputDoc from "../pages/docs/Components/InputDoc";
 import LoadersDoc from "../pages/docs/Animations/LoadersDoc";
 import BentoDoc from "../pages/docs/Components/BentoDoc";
 import CardDoc from "../pages/docs/Components/CardDoc";
@@ -7,7 +7,7 @@ import Introduction from "../pages/docs/GettingStarted/Introduction";
 
 export const ComponentMap = {
   button: ButtonDoc,
-  input: InputDoc,
+  // input: InputDoc,
   loaders: LoadersDoc,
   bento: BentoDoc,
   cards: CardDoc,
