80fbf1b717372bd0d3d746a4d0d8c8402375e711
Tushar Sahu <tusharsahu190702@gmail.com>
2024-10-16 23:12:51 +0530

    feat : framer animation added to grid (home page)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -111,28 +111,117 @@ const Home = () => {
       </motion.h1>
 
       {/* background pattern designs */}
-      <div className="lg:grid grid-cols-4 hidden  absolute top-[130px] w-full   left-1/2 -translate-x-1/2">
-        <div className="  h-[80px] "></div>
-        <div className="border-t border-white/10 border-b h-[80px] col-span-2"></div>
-        <div className=" border-white/10  h-[80px]"></div>
-        <div className="border-b border-white/10 h-[130px]"></div>
-        <div className="border-b border-white/10 h-[130px] col-span-2"></div>
-        <div className="border-b border-white/10 h-[130px]"></div>
-        <div className="border-b border-white/10 h-60"></div>
-        <div className="border-b border-white/10 h-60 col-span-2"></div>
-        <div className="border-b border-white/10 h-60"></div>
-        <div className=" border-white/10 h-[80px]"></div>
-        <div className="border-b border-white/10 rounded-3xl h-[80px] col-span-2"></div>
-        <div className=" border-white/10 h-[80px]"></div>
-      </div>
-      <div className="lg:grid grid-cols-4 hidden  mt-[57px] absolute top-0 w-full  left-1/2 -translate-x-1/2">
-        <div className="h-[603px]"></div>
-        <div className="col-span-2 rounded-b-3xl  border-l border-r border-white/10 grid grid-cols-8 ">
-          <div></div>
-          <div className="border-r  border-l border-white/10 col-span-6"></div>
-        </div>
-        <div className="h-[603px] "></div>
-      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className="lg:grid grid-cols-4 hidden  absolute top-[130px] w-full   left-1/2 -translate-x-1/2"
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="h-[80px]"
+        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="border-t border-white/10 border-b h-[80px] col-span-2"
+        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className=" border-white/10  h-[80px]"
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="border-b border-white/10 h-[130px]"
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="border-b border-white/10 h-[130px] col-span-2"
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="border-b border-white/10 h-[130px]"
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="border-b border-white/10 h-60"
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="border-b border-white/10 h-60 col-span-2"
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="border-b border-white/10 h-60"
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className=" border-white/10 h-[80px]"
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="border-b border-white/10 rounded-3xl h-[80px] col-span-2"
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className=" border-white/20 h-[80px]"
+        ></motion.div>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3, delay: 0.2 }}
+        className="lg:grid grid-cols-4 hidden  mt-[63px] absolute top-0 w-full  left-1/2 -translate-x-1/2"
+      >
+        <motion.div
+          // initial={{ opacity: 0 }}
+          // animate={{ opacity: 1 }}
+          // transition={{ duration: 0.3, delay: 0.2 }}
+        ></motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.2 }}
+          className="col-span-2 rounded-b-3xl  border-l border-r border-white/10 grid grid-cols-8 "
+        >
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3, delay: 0.2 }}
+          ></motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3, delay: 0.2 }}
+            className="border-r  border-l border-white/10 col-span-6"
+          ></motion.div>
+        </motion.div>
+        <div className="h-[596px] "></div>
+      </motion.div>
     </div>
   );
 };
