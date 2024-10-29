a40900378d802e61d0b35d38cb00abb915e9a651
Tushar Sahu <tusharsahu190702@gmail.com>
2024-10-29 18:31:28 +0530

    refactor : updated the design and added the signup form

--- src/components/molecules/Authentication.tsx
+++ src/components/molecules/Authentication.tsx
@@ -7,6 +7,8 @@ const Authentication = () => {
   const [visibleEye, setVisibleEye] = useState(true);
   const [inputVisible, setInputVisible] = useState("password");
 
+  const [show, setShow] = useState(false);
+
   const handleVisibleEye = () => {
     setVisibleEye(!visibleEye);
     if (inputVisible === "password") {
@@ -50,34 +52,90 @@ const Authentication = () => {
         <div className="circle absolute top-48 left-44 w-20 h-20 bg-purple-500 rounded-full filter"></div>
         <div className="circle absolute top-96 right-24 w-40 h-40 bg-orange-500 rounded-full filter"></div>
         <div className="h-[550px] w-[400px]  shadow-lg rounded-3xl py-5 px-12 border-2 border-white/20  flex flex-col gap-2 items-center justify-center backdrop-filter backdrop-blur-[20px] bg-opacity-5 bg-white transform ">
-          <h1 className=" text-2xl font-semibold mb-3">Log in</h1>
-          <input
-            type="text"
-            className=" bg-transparent px-4 py-2 border border-white/20 rounded-md w-full"
-            placeholder="Phone, email or username"
-          />
-          <div className="flex items-center justify-center px-2  border border-white/20 focus-within:outline-1 focus-within:outline rounded-md w-full">
-            <input
-              type={inputVisible}
-              className=" bg-transparent px-2 py-2 focus:outline-none w-full"
-              placeholder="Password"
-            />
-            {visibleEye ? (
-              <Eye onClick={handleVisibleEye} className=" cursor-pointer" />
-            ) : (
-              <EyeOff onClick={handleVisibleEye} className=" cursor-pointer" />
-            )}
-          </div>
-          <Button
-            variant="primary"
-            className="w-full bg-[#8046b6] hover:bg-[#512c74] my-3 py-3"
-          >
-            Log in
-          </Button>
-          <p>
-            Don't have an account?{" "}
-            <span className="text-[#925cbe] cursor-pointer">Signup</span>
-          </p>
+          {show ? (
+            <>
+              <h1 className=" text-2xl font-semibold mb-3">Signup</h1>
+              <input
+                type="text"
+                className=" bg-transparent px-4 py-2 border border-white/20 rounded-md w-full"
+                placeholder="email"
+              />
+              <input
+                type="text"
+                className=" bg-transparent px-4 py-2 border border-white/20 rounded-md w-full"
+                placeholder="username"
+              />
+              <div className="flex items-center justify-center px-2  border border-white/20 focus-within:outline-1 focus-within:outline rounded-md w-full">
+                <input
+                  type={inputVisible}
+                  className=" bg-transparent px-2 py-2 focus:outline-none w-full"
+                  placeholder="Password"
+                />
+                {visibleEye ? (
+                  <Eye onClick={handleVisibleEye} className=" cursor-pointer" />
+                ) : (
+                  <EyeOff
+                    onClick={handleVisibleEye}
+                    className=" cursor-pointer"
+                  />
+                )}
+              </div>
+              <Button
+                variant="primary"
+                className="w-full bg-[#8046b6] hover:bg-[#512c74] my-3 py-3"
+              >
+                Signup
+              </Button>
+              <p>
+                Already have an account?{" "}
+                <span
+                  className="text-blue-500/60 cursor-pointer"
+                  onClick={() => setShow(false)}
+                >
+                  Log in
+                </span>
+              </p>
+            </>
+          ) : (
+            <>
+              <h1 className=" text-2xl font-semibold mb-3">Log in</h1>
+              <input
+                type="text"
+                className=" bg-transparent px-4 py-2 border border-white/20 rounded-md w-full"
+                placeholder="Phone, email or username"
+              />
+              <div className="flex items-center justify-center px-2  border border-white/20 focus-within:outline-1 focus-within:outline rounded-md w-full">
+                <input
+                  type={inputVisible}
+                  className=" bg-transparent px-2 py-2 focus:outline-none w-full"
+                  placeholder="Password"
+                />
+                {visibleEye ? (
+                  <Eye onClick={handleVisibleEye} className=" cursor-pointer" />
+                ) : (
+                  <EyeOff
+                    onClick={handleVisibleEye}
+                    className=" cursor-pointer"
+                  />
+                )}
+              </div>
+              <Button
+                variant="primary"
+                className="w-full bg-[#8046b6] hover:bg-[#512c74] my-3 py-3"
+              >
+                Log in
+              </Button>
+              <p>
+                Don't have an account?{" "}
+                <span
+                  className="text-blue-500/60 cursor-pointer"
+                  onClick={() => setShow(true)}
+                >
+                  Signup
+                </span>
+              </p>
+            </>
+          )}
         </div>
       </div>
     </div>
