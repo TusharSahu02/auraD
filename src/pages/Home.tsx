c945f6c4b349077f0feb9a27b9ddcabfa3e20e27
TusharSahu02 <tusharsahu190702@gmail.com>
2024-12-01 21:14:12 +0530

    fix : overflow x on scrollTranslate component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,21 +36,21 @@ const Home = () => {
 
   return (
     <>
-      <div className="bg-[#090716] w-full min-h-screen relative">
+      <div className="relative min-h-screen w-full bg-[#090716]">
         <Navbar />
-        <div className=" py-2 lg:px-10 flex items-center flex-col justify-center z-20 ">
-          <div className=" flex px-10 items-center flex-col justify-center pt-24 text-white ">
+        <div className="z-20 flex flex-col items-center justify-center py-2 lg:px-10">
+          <div className="flex flex-col items-center justify-center px-10 pt-24 text-white">
             <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.2 }}
               className="flex items-center gap-x-3"
             >
-              <div className="h-[1px] bg-gradient-to-r from-gray-900 to-[#ffffffaa] w-[78px]" />
-              <h1 className=" font-geistLight text-xl text-gray-400  shiny-text">
+              <div className="h-[1px] w-[78px] bg-gradient-to-r from-gray-900 to-[#ffffffaa]" />
+              <h1 className="shiny-text font-geistLight text-xl text-gray-400">
                 Introducing
               </h1>
-              <div className="h-[1px] bg-gradient-to-r from-[#ffffffaa] to-gray-900 w-[78px]" />
+              <div className="h-[1px] w-[78px] bg-gradient-to-r from-[#ffffffaa] to-gray-900" />
             </motion.div>
             {/* <motion.h1
             initial={{ opacity: 0, y: 20 }}
@@ -61,7 +61,7 @@ const Home = () => {
             auraD
           </motion.h1> */}
             <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out">
-              <span className="font-geistBlack text-[100px] mt-1 leading-[135px]">
+              <span className="mt-1 font-geistBlack text-[100px] leading-[135px]">
                 auraD
               </span>
             </AnimatedShinyText>
@@ -77,7 +77,7 @@ const Home = () => {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.6 }}
-              className="lg:text-4xl mt-10 md:text-3xl font-geistMedium capitalize text-2xl text-center"
+              className="mt-10 text-center font-geistMedium text-2xl capitalize md:text-3xl lg:text-4xl"
             >
               Design. Develop. Deploy.
             </motion.p>
@@ -85,7 +85,7 @@ const Home = () => {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.8 }}
-              className="text-gray-500 text-center mt-5 lg:w-[35%] xl:w-[45%] md:w-1/2"
+              className="mt-5 text-center text-gray-500 md:w-1/2 lg:w-[35%] xl:w-[45%]"
             >
               Create stunning React websites in hours, not months, with AuraD
               UI's sleek and animated components. Optimized for speed, simply
@@ -96,27 +96,27 @@ const Home = () => {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 1 }}
-              className="xl:mt-[85px] z-20  mt-[65px]"
+              className="z-20 mt-[65px] xl:mt-[85px]"
             >
               <Link to="/docs/reactjs/components/button">
                 <button
                   type="button"
-                  className="border text-sm font-medium relative border-white/[0.2]  text-white px-4 py-2 rounded-full group"
+                  className="group relative rounded-full border border-white/[0.2] px-4 py-2 text-sm font-medium text-white"
                 >
-                  <span className="absolute inset-x-0 w-1/2 mx-auto  transition-opacity duration-300  -top-px bg-gradient-to-r opacity-0 group-hover:opacity-100  from-transparent via-blue-500 to-transparent  h-px" />
+                  <span className="absolute inset-x-0 -top-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
                   <span>Browse Components</span>
-                  <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+                  <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
                 </button>
               </Link>
             </motion.div>
           </div>
 
           {/*  PerspectiveGrid  */}
-          <div className=" w-full mt-20 ">
+          <div className="mt-20 w-full">
             <div className="z-10 flex items-center justify-center">
               <div
                 className={cn(
-                  "group rounded-full border-b  text-base text-white transition-all ease-in    border-white/5 bg-[#090716]"
+                  "group rounded-full border-b border-white/5 bg-[#090716] text-base text-white transition-all ease-in",
                 )}
               >
                 <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out">
@@ -125,16 +125,16 @@ const Home = () => {
               </div>
             </div>
             <ZoomParallax>
-              <div className="flex items-center justify-center mt-28">
-                <div className=" xl:w-[70%] 2xl:w-[60%] w-full px-10 grid lg:grid-cols-3 gap-6">
+              <div className="mt-28 flex items-center justify-center">
+                <div className="grid w-full gap-6 px-10 lg:grid-cols-3 xl:w-[70%] 2xl:w-[60%]">
                   {components.map((item, index) => (
-                    <div key={index} className="relative ">
-                      <div className="h-[200px] bg-gray-900/20 flex items-center justify-center border border-white/10 rounded-xl">
+                    <div key={index} className="relative">
+                      <div className="flex h-[200px] items-center justify-center rounded-xl border border-white/10 bg-gray-900/20">
                         <div className="absolute z-20">{item?.component}</div>
                       </div>
                       <DotPattern
                         className={cn(
-                          "[mask-image:radial-gradient(135px_circle_at_center,white,transparent)]  z-10 "
+                          "z-10 [mask-image:radial-gradient(135px_circle_at_center,white,transparent)]",
                         )}
                       />
                     </div>
@@ -145,17 +145,17 @@ const Home = () => {
           </div>
 
           {/* Scroll Translate GSAP */}
-          <div className=" lg:mt-[800px] mt-[2080px]">
+          <div className="mt-[2080px] overflow-x-hidden lg:mt-[800px]">
             <ScrollTranslateHome />
           </div>
 
           {/* bento grid components */}
-          <div className=" xl:w-[70%] 2xl:w-[60%] px-2">
+          <div className="px-2 xl:w-[70%] 2xl:w-[60%]">
             <Bento />
           </div>
 
           <motion.div
-            className="w-[80%] flex items-center justify-center my-6"
+            className="my-6 flex w-[80%] items-center justify-center"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 1.4 }}
@@ -163,17 +163,17 @@ const Home = () => {
             <Link to="/docs/reactjs/components/button">
               <button
                 type="button"
-                className="border text-sm font-medium relative border-white/[0.2] text-white px-4 py-2 rounded-full group"
+                className="group relative rounded-full border border-white/[0.2] px-4 py-2 text-sm font-medium text-white"
               >
-                <span className="absolute inset-x-0 w-1/2 mx-auto transition-opacity duration-300 -top-px bg-gradient-to-r opacity-0 group-hover:opacity-100 from-transparent via-blue-500 to-transparent h-px" />
+                <span className="absolute inset-x-0 -top-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
                 <span>See more</span>
-                <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
+                <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
               </button>
             </Link>
           </motion.div>
         </div>
 
-        <div className=" pt-10 hidden md:block">
+        <div className="hidden pt-10 md:block">
           <TextHoverEffect text="AURAD" />
         </div>
 
@@ -181,7 +181,7 @@ const Home = () => {
           initial={{ opacity: 0, y: 0 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 3 }}
-          className="text-gray-400 text-center text-sm py-10 w-full "
+          className="w-full py-10 text-center text-sm text-gray-400"
         >
           All rights reserved © 2024 auraD |
           <span className="ml-1">
@@ -189,7 +189,7 @@ const Home = () => {
               href="https://x.com/BuddyHidden"
               target="_blank"
               rel="noopener noreferrer"
-              className=" underline text-gray-300 underline-offset-2"
+              className="text-gray-300 underline underline-offset-2"
             >
               Tushar Sahu
             </a>
